11f2da9dd95f639a5e39ce8367c83f38141b7636
ppilatso <pilatsophenyo@gmail.com>
2023-03-01 20:45:06 +0200

    fianl

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,132 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import "@aws-amplify/ui-react/styles.css";
 import {
-  withAuthenticator,
   Button,
+  Flex,
   Heading,
   Image,
+  Text,
+  TextField,
   View,
-  Card,
-} from '@aws-amplify/ui-react'
+  withAuthenticator,
+} from "@aws-amplify/ui-react";
+import { listNotes } from './graphql/queries';
+import { 
+  createNote as createNoteMutation, 
+  deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { useEffect, useState } from 'react';
+import { API, Storage } from 'aws-amplify';
 
-function App() {
 
-  const signOut = () => {
-    alert('Lets Go!!!!!!!!');
+function App ({ signOut }: any) {
+
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData: any = await API.graphql({ query: listNotes });
+    const notesFromApi: any = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromApi.map(async (note: any) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
+    setNotes(notesFromApi);
+  }
+
+  async function createNote(event: any) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const image: any = form.get("image");
+    const data: any = {
+      name: form.get("name"),
+      description: form.get("description"),
+      image: image.name,
+    };
+    if (!!data.image) await Storage.put(data.name, image);
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: data },
+    });
+    fetchNotes();
+    event.target.reset();
+  }
+
+  async function deleteNote({ id, name }: any) {
+    const newNotes = notes.filter((note: any) => note.id !== id);
+    setNotes(newNotes);
+    await Storage.remove(name);
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
   }
 
   return (
-    <View className='App'>
-      <Card>
-        <Image src={reactLogo} className='App-logo' alt='logo'/>
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+    <View className="App"
+    name="image"
+    as="input"
+    type="file"
+    style={{ alignSelf: "end" }}>
+      <Heading level={1}>My Notes App</Heading>
+      <View as="form" margin="3rem 0" onSubmit={createNote}>
+        <Flex direction="row" justifyContent="center">
+          <TextField
+            name="name"
+            placeholder="Note Name"
+            label="Note Name"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="description"
+            placeholder="Note Description"
+            label="Note Description"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <Button type="submit" variation="primary">
+            Create Note
+          </Button>
+        </Flex>
+      </View>
+      <Heading level={2}>Current Notes</Heading>
+      <View margin="3rem 0">
+
+      {notes.map((note: any) => (
+        <Flex
+          key={note.id || note.name}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text as="strong" fontWeight={700}>
+            {note.name}
+          </Text>
+          <Text as="span">{note.description}</Text>
+          {note.image && (
+            <Image
+              src={note.image}
+              alt={''}
+              style={{ width: 400 }}
+            />
+          )}
+          <Button variation="link" onClick={() => deleteNote(note)}>
+            Delete note
+          </Button>
+        </Flex>
+      ))}
+
+      </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   )
--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -23,7 +23,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://jow435bxv5akra3gqre6oxu6dy.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-zk73vr7pzbcvzfedpecgwyou2q",
+    "aws_user_files_s3_bucket": "launcher618ae131b5a24e17b9617e453ad6b3a5181606-staging",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
