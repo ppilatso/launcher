25c1dcf010042a6845a9143c421aa829b912dd5f
ppilatso <pilatsophenyo@gmail.com>
2023-03-01 14:55:39 +0200

    fix build failures

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,20 @@ export default defineConfig({
   define: {
     global: {}
   },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
