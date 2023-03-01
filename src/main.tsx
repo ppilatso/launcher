89e43f69c9e01460e8b302e026ad83a7a10a65be
ppilatso <pilatsophenyo@gmail.com>
2023-03-01 10:24:47 +0200

    auth added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
 import App from './App'
 import './index.css'
+
+
+Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
